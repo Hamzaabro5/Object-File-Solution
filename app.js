58eb73259ca17731174494de1ddd7893b21430b5
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-06 21:56:34 +0500

    Object file assignment

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 console.log(`Hello Object`);
 
+const h1  = document.querySelector(`.head1`)
+const h2  = document.querySelector(`.head2`)
+
+
+// // Answer No 1
+
+
+// var itemsArray = [
+//     {
+//     name: `juice`, 
+//     price: 50, 
+//     quantity: 3,
+//     },
+//     {
+//         name: `cookie`, 
+//         price: 30 ,
+//         quantity: 9,
+//     },
+
+//     {
+//         name: `shirt`,
+//         price: 880 ,
+//         quantity: 1,
+//     },
+
+//     {
+//         name: `pen` ,
+//         price: 100 ,
+//         quantity: 2,
+//     },
+// ];
+
+// console.log(itemsArray[0].price*itemsArray[0].quantity);
+// console.log(itemsArray[1].price*itemsArray[1].quantity);
+// console.log(itemsArray[2].price*itemsArray[2].quantity);
+// console.log(itemsArray[3].price*itemsArray[3].quantity);
+
+// h2.innerHTML = `<ul>
+// <li>Total Price of 3 juice is ${itemsArray[0].price*itemsArray[0].quantity}</li>
+// <li>Total Price of 9 cookie is ${itemsArray[1].price*itemsArray[1].quantity}</li>
+// <li>Total Price of 1 shirt is ${itemsArray[2].price*itemsArray[2].quantity}</li>
+// <li>Total Price of 2 pen is ${itemsArray[3].price*itemsArray[3].quantity}</li>
+
+
+// </ul>`
+
 
 // Answer No 1
 
-var itemsArray = [
-    {name:`juice` , price: `50` , quantity: `3`},
-    {name:`cookie` , price: `30` , quantity: `9`},
-    {name:`shirt` , price: `880` , quantity: `1`},
-    {name:`pen` , price: `100` , quantity: `2`},
-];
 
-// Answer No 1
+
+
+
+
+
+
+
+// Answer No 2
+
+
+
+// Answer No 2
