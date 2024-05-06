da20e7edb5c44774e24e9bc66c466af3047d0e94
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-06 21:20:46 +0500

    Object File Assignment

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(`Hello Object`);
+
+
+// Answer No 1
+
+var itemsArray = [
+    {name:`juice` , price: `50` , quantity: `3`},
+    {name:`cookie` , price: `30` , quantity: `9`},
+    {name:`shirt` , price: `880` , quantity: `1`},
+    {name:`pen` , price: `100` , quantity: `2`},
+];
+
+// Answer No 1
