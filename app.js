eefba07a6a7e1399af971fb8cbb22d7f34a595f2
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-07 17:25:24 +0500

    Object File Assignment

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ console.log(`Hello Object`);
 
 const h1  = document.querySelector(`.head1`)
 const h2  = document.querySelector(`.head2`)
+const h3  = document.querySelector(`.head3`)
+const h4  = document.querySelector(`.head4`)
+const h5  = document.querySelector(`.head5`)
 
 
 // // Answer No 1
@@ -77,3 +80,84 @@ const h2  = document.querySelector(`.head2`)
 // console.log(obj[0].lastName);
 
 // Answer No 2
+
+
+
+
+
+
+
+
+
+// // Answer No 3
+
+// const obj = {
+//             name: `Muhammad Hamza`,
+//             age: 18,
+//             email: `hamzaabro683@gmail.com`,
+//             gender: `male`,
+//             city: `Karachi`,
+//             country: `Pakistan`
+    
+//         }
+
+
+// function createObj(name , age , email , gender , city , country) {
+//     this.name = name;
+//     this.age = age;
+//     this.email = email;
+//     this.gender = gender;
+//     this.city = city;
+//     this.country = country;
+// }
+
+
+// const objOne = new createObj(`Abdullah` , 18 , `abdullahahmed@gmail.com` , `male` , `Karachi` , `Pakistan`)
+
+
+// console.log(obj);
+// console.log(objOne);
+
+// // Answer No 3
+
+
+
+
+
+
+
+
+
+
+// // Answer No 4
+
+// const inputOne = document.querySelector(`.inputOne`)
+// const inputTwo = document.querySelector(`.inputTwo`)
+// const inputThree = document.querySelector(`.inputThree`)
+// const inputFour = document.querySelector(`.inputFour`)
+// const inputFive = document.querySelector(`.inputFive`)
+
+// function createObj() {
+//     const obj = {
+//         name: inputOne.value ,
+//         gender: inputTwo.value ,
+//         adrress: inputThree.value ,
+//         education: inputFour.value ,
+//         profession: inputFive.value
+
+//     }
+//     h1.innerHTML = `<h1>${obj.name}</h1> `
+//     h2.innerHTML = `<h1>${obj.gender}</h1> `
+//     h3.innerHTML = `<h1>${obj.adrress}</h1> `
+//     h4.innerHTML = `<h1>${obj.education}</h1> `
+//     h5.innerHTML = `<h1>${obj.profession}</h1> `
+    
+    
+//     console.log(obj);
+//     inputOne.value = ``
+//     inputTwo.value = ``
+//     inputThree.value = ``
+//     inputFour.value = ``
+//     inputFive.value = ``
+// }
+// // Answer No 4
